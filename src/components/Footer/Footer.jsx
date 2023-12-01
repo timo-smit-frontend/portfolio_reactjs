9b09b9f01e27b6dc618c94c1a2902f26a429fedb
Capgemini-Timo <timo.smit@capgemini.com>
2023-12-01 11:55:04 +0100

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import logoTS from "../../assets/ts_logo.png";
+import "./Footer.css";
+
+export default function Footer() {
+  return;
+  <footer>
+    <img src={logoTS} alt=""></img>
+    <div className="contact">
+      <h3>Contact</h3>
+      <a href="mailto:timo.smit@live.nl">timo.smit@live.nl</a>
+      <a href="tel:+31610792320">+31 6 10792320</a>
+      <a href="https://www.linkedin.com/in/timo-smit-09983b14a/">LinkedIn</a>
+      <a href="https://timo-smit.webflow.io/">
+        Communication and Mulimedia Design Portfolio
+      </a>
+    </div>
+  </footer>;
+}
